61afd001bc60853d28730ae9146cd7246da445a1
André König <andre.koenig@openformation.io>
2024-03-24 18:45:04 +0100

    feat: adds fly.io deployment

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/openformation/plausible-proxy
 go 1.22.1
 
 require github.com/go-chi/chi/v5 v5.0.12
+
+require github.com/go-chi/cors v1.2.1 // indirect
--- main.go
+++ main.go
@@ -5,65 +5,163 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
-var LISTEN_ADDR = os.Getenv("LISTEN_ADDR")
+type Environment struct {
+	LISTEN_ADDRESS       string
+	CORS_ORIGINS         []string
+	PLAUSIBLE_SCRIPT_URL string
+	PLAUSIBLE_API_URL    string
+}
 
-func getScript(w http.ResponseWriter, r *http.Request) {
-	scriptExtension := chi.URLParam(r, "name")
+func ParseEnvironment() *Environment {
+	LISTEN_ADDRESS := os.Getenv("LISTEN_ADDRESS")
+	PLAUSIBLE_SCRIPT_URL := os.Getenv("PLAUSIBLE_SCRIPT_URL")
+	PLAUSIBLE_API_URL := os.Getenv("PLAUSIBLE_API_URL")
+	CORS_ORIGINS := os.Getenv("CORS_ORIGINS")
 
-	url := fmt.Sprintf("https://plausible.io/js/%s", scriptExtension)
-
-	response, err := http.Get(url)
-
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("Internal Server Error"))
-
-		return
+	if LISTEN_ADDRESS == "" {
+		LISTEN_ADDRESS = "localhost:8080"
 	}
 
-	if response.StatusCode != http.StatusOK {
+	if PLAUSIBLE_SCRIPT_URL == "" {
+		PLAUSIBLE_SCRIPT_URL = "https://plausible.io/js/%s"
+	}
+
+	if PLAUSIBLE_API_URL == "" {
+		PLAUSIBLE_API_URL = "https://plausible.io/api/event"
+	}
+
+	if CORS_ORIGINS == "" {
+		panic("CORS_ORIGINS environment variable is required")
+	}
+
+	PARSED_CORS_ORIGINS := strings.Split(CORS_ORIGINS, ",")
+
+	return &Environment{LISTEN_ADDRESS: LISTEN_ADDRESS, PLAUSIBLE_SCRIPT_URL: PLAUSIBLE_SCRIPT_URL, PLAUSIBLE_API_URL: PLAUSIBLE_API_URL, CORS_ORIGINS: PARSED_CORS_ORIGINS}
+}
+
+func buildGetScriptHandler(plausibleScriptUrl string) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		scriptExtension := chi.URLParam(r, "name")
+
+		url := fmt.Sprintf(plausibleScriptUrl, scriptExtension)
+
+		response, error := http.Get(url)
+
+		if error != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(http.StatusText(http.StatusInternalServerError)))
+
+			return
+		}
+
+		if response.StatusCode != http.StatusOK {
+			w.WriteHeader(response.StatusCode)
+			w.Write([]byte(http.StatusText(response.StatusCode)))
+
+			return
+		}
+
+		defer response.Body.Close()
+
+		// Copying headers from the origin request to the response
+		for key, values := range response.Header {
+			for _, value := range values {
+				w.Header().Add(key, value)
+			}
+		}
+
+		// Copying the status code from the origin request to the response
 		w.WriteHeader(response.StatusCode)
-		w.Write([]byte(http.StatusText(response.StatusCode)))
 
-		return
-	}
+		// Copying the body from the origin request to the response
+		_, error = io.Copy(w, response.Body)
 
-	defer response.Body.Close()
-
-	// Copying headers from the origin request to the response
-	for key, values := range response.Header {
-		for _, value := range values {
-			w.Header().Add(key, value)
+		if error != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(http.StatusText(http.StatusInternalServerError)))
 		}
 	}
+}
 
-	// Copying the status code from the origin request to the response
-	w.WriteHeader(response.StatusCode)
+func buildPostEventHandler(plausibleApiUrl string) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		request, error := http.NewRequest(r.Method, plausibleApiUrl, r.Body)
 
-	// Copying the body from the origin request to the response
-	_, err = io.Copy(w, response.Body)
+		if error != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(http.StatusText(http.StatusInternalServerError)))
 
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("Internal Server Error"))
+			return
+		}
+
+		for key, values := range r.Header {
+			for _, value := range values {
+				fmt.Println(key, value)
+				// Let's not copy the Cookie header
+				if key != "Cookie" {
+					request.Header.Add(key, value)
+				}
+			}
+		}
+
+		request.Header.Set("X-Forwarded-For", r.RemoteAddr)
+
+		client := http.DefaultClient
+		response, error := client.Do(request)
+
+		if error != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(http.StatusText(http.StatusInternalServerError)))
+
+			return
+		}
+
+		defer response.Body.Close()
+
+		// Copying headers from the origin request to the response
+		for key, values := range response.Header {
+			for _, value := range values {
+				w.Header().Add(key, value)
+			}
+		}
+
+		// Copying the status code from the origin request to the response
+		w.WriteHeader(response.StatusCode)
+
+		// Copying the body from the origin request to the response
+		_, error = io.Copy(w, response.Body)
+
+		if error != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(http.StatusText(http.StatusInternalServerError)))
+		}
 	}
 }
 
 func main() {
-	var r = chi.NewRouter()
+	r := chi.NewRouter()
+
+	env := ParseEnvironment()
 
 	r.Use(middleware.Logger)
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   env.CORS_ORIGINS,
+		AllowedMethods:   []string{"GET", "POST", "OPTIONS"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
 
-	r.Get("/js/{name}", getScript)
+	r.Get("/js/{name}", buildGetScriptHandler(env.PLAUSIBLE_SCRIPT_URL))
+	r.Post("/api/event", buildPostEventHandler(env.PLAUSIBLE_API_URL))
 
-	if LISTEN_ADDR == "" {
-		LISTEN_ADDR = "localhost:8080"
-	}
+	LISTEN_ADDRESS := (*env).LISTEN_ADDRESS
 
-	http.ListenAndServe(LISTEN_ADDR, r)
+	http.ListenAndServe(LISTEN_ADDRESS, r)
 }
