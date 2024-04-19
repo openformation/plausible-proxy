6841a477e56656e48d1c88eff5dae4760f718cce
André König <andre.koenig@openformation.io>
2024-04-19 19:06:19 +0200

    chore: adds debug statement

--- main.go
+++ main.go
@@ -105,6 +105,8 @@ func buildPostEventHandler(plausibleApiUrl string) func(w http.ResponseWriter, r
 		// during the request flow. The first one will be the actual client IP address
 		// (the one we're interested in). The other ones will be the intermediate proxies.
 		xForwardedForHeader := r.Header.Get("X-Forwarded-For")
+		println("X-Forwarded-For")
+		println(xForwardedForHeader)
 		xForwardedForHeaderIpAddresses := strings.Split(xForwardedForHeader, ",")
 		firstIpAddress := strings.Trim(xForwardedForHeaderIpAddresses[0], " ")
 
